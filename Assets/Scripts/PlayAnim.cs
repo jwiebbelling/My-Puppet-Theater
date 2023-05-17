0a1a0607cd2a6e272f0e57d34aff86eb764d8113
j.wiebbelling <j.wiebbelling@sintlucasedu.nl>
2023-05-17 11:21:17 +0300

    Robijn can be drinked

--- Assets/Scripts/PlayAnim.cs
+++ Assets/Scripts/PlayAnim.cs
@@ -7,5 +7,12 @@ public class PlayAnim : MonoBehaviour
     private void OnMouseDown()
     {
         GetComponent<Animator>().SetBool("IsClicked", true);
+        StartCoroutine(Example());
+    }
+
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(3);
+        GetComponent<Animator>().SetBool("IsClicked", false);
     }
 }
